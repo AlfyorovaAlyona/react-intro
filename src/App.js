c463c6de7e18faa8ca443929fe0c2557dfd03dae
Alyona Alfyorova <alfyorova.av@phystech.edu>
2019-03-20 00:01:39 +0300

    starting react TODO

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import './App.css';
+import Header from "./Header";
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+function App() {
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+
+  return (
+      <div className={"todo-list"}>
+        <Header/>
+        {todoItems}
       </div>
-    );
-  }
+  )
 }
 
 export default App;
